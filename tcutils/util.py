910adcf950def06492891cc35efbea70b9f212b3
Senthilnathan Murugappan <msenthil@juniper.net>
2016-05-23 17:12:22 -0700

    Moving CI sanity testcases to contrail-test-ci repo Also adding lbaas tests to ci-sanity
    Change-Id: Ia7d33c6958e2b0e2838b990956b4bfb48ef3591e

--- tcutils/util.py
+++ tcutils/util.py
@@ -273,13 +273,10 @@ def run_cmd_through_node(host_string, cmd, password=None, gateway=None,
         tries = 1
         output = None
         while tries > 0:
-            if timeout:
-                try:
-                    output = _run(cmd, timeout=timeout)
-                except CommandTimeout:
-                    pass
-            else:
-                output = _run(cmd)
+            try:
+                output = _run(cmd, timeout=timeout, pty=not as_daemon)
+            except CommandTimeout:
+                pass
             if (output) and ('Fatal error' in output):
                 tries -= 1
                 time.sleep(5)
