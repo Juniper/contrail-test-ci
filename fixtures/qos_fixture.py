88afa129590ffcc101b913bf202710d586f20be6
Pulkit <pulkitt@juniper.net>
2016-11-16 17:47:03 +0530

    Incorrect call for parent object in qos fixture. Corrected it.
    Change-Id: I540f05bbe29b6433bdeb0b7c4ecd0761893b2838

--- fixtures/qos_fixture.py
+++ fixtures/qos_fixture.py
@@ -31,7 +31,12 @@ class QosBaseFixture(vnc_api_test.VncLibFixture):
 
     def get_parent_obj(self):
         if getattr(self, 'qos_config_type', None) == 'project':
-            parent_obj = super(QosBaseFixture, self).get_project_obj(self)
+            if self.connections:
+                project_id = self.connections.project_id
+            else:
+                project_id = self.vnc_api_h.project_read(
+                    fq_name_str='default-domain:default-project').uuid
+            parent_obj = self.vnc_api_h.project_read(id=project_id)
         else:
             parent_obj = self.vnc_api_h.global_qos_config_read(fq_name_str=
                                                                global_qos_config_fq_str)
