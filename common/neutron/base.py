148b5024b898b1ce16942574e37555e1de28a94a
Ganesha H V <ganeshahv@juniper.net>
2016-11-28 21:16:29 +0530

    Changes in AAP testcases
    1]. Using a single function to configure AAP
    2]. Moved the VNC API config part to contrailapi
    3]. Added safe_run and safe_sudo to handle ChannelException
    
    Change-Id: I2684176e6945b1cdb48c8acf2d12d84c68bee04c

--- common/neutron/base.py
+++ common/neutron/base.py
@@ -418,42 +418,6 @@ class BaseNeutronTest(test_v1.BaseTestCase_v1):
         return True
     # end config_aap
 
-    def config_aap_contrail (self, port1, port2, ip, vsrx=False, zero_mac=False):
-        if zero_mac:
-            port1_dict = {'allowed_address_pairs': [
-                {"ip_address": ip + '/32', "mac_address": '00:00:00:00:00:00'}]}
-            port2_dict = {'allowed_address_pairs': [
-                {"ip_address": ip + '/32', "mac_address": '00:00:00:00:00:00'}]}
-        else:
-            if vsrx:
-                port1_dict = {'allowed_address_pairs': [
-                    {"ip_address": ip + '/32', "mac_address": '00:00:5e:00:01:01'}]}
-                port2_dict = {'allowed_address_pairs': [
-                    {"ip_address": ip + '/32', "mac_address": '00:00:5e:00:01:01'}]}
-            else:
-                port1_dict = {'allowed_address_pairs': [
-                    {"ip_address": ip + '/32'}]}
-                port2_dict = {'allowed_address_pairs': [
-                    {"ip_address": ip + '/32'}]}
-        ip1 = SubnetType(ip_prefix=port1_dict['allowed_address_pairs'][0]['ip_address'].split('/')[0],\
-                         ip_prefix_len=int(port1_dict['allowed_address_pairs'][0]['ip_address'].split('/')[1]))
-        aap1 = AllowedAddressPair(ip=ip1, mac=port1_dict['allowed_address_pairs'][0]['mac_address'])
-        aaps1 = AllowedAddressPairs(allowed_address_pair=[aap1])
-        self.vmi1_obj = self.vnc_lib.virtual_machine_interface_read(id=port1['id'])
-        #self.vmi1_obj.set_virtual_machine_interface_allowed_address_pairs([aaps1])
-        self.vmi1_obj.set_virtual_machine_interface_allowed_address_pairs(aaps1)
-
-        ip2 = SubnetType(ip_prefix=port2_dict['allowed_address_pairs'][0]['ip_address'].split('/')[0],\
-                         ip_prefix_len=int(port2_dict['allowed_address_pairs'][0]['ip_address'].split('/')[1]))
-        aap2 = AllowedAddressPair(ip=ip2, mac=port2_dict['allowed_address_pairs'][0]['mac_address'])
-        aaps2 = AllowedAddressPairs(allowed_address_pair=[aap2])
-        self.vmi2_obj = self.vnc_lib.virtual_machine_interface_read(id=port2['id'])
-        #self.vmi2_obj.set_virtual_machine_interface_allowed_address_pairs([aaps2])
-        self.vmi2_obj.set_virtual_machine_interface_allowed_address_pairs(aaps2)
-        self.vnc_lib.virtual_machine_interface_update(self.vmi1_obj)
-        self.vnc_lib.virtual_machine_interface_update(self.vmi2_obj)
-        return True 
-
     def config_vrrp_on_vsrx(self, vm_fix, vip, priority):
         cmdList = []
         cmdList.append('deactivate security nat source rule-set TestNat')
