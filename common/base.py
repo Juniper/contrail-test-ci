4f11913a83a4974f15763effb7f4d8f7a510d0f2
Ganesha H V <ganeshahv@juniper.net>
2016-12-05 21:30:10 +0530

    Support for AAP active-active mode
    Change-Id: I251075ffbafc1517c3ce88ea032eca502f445f1e

--- common/base.py
+++ common/base.py
@@ -7,6 +7,7 @@ from policy_test import PolicyFixture
 from port_fixture import PortFixture
 from interface_route_table_fixture import InterfaceRouteTableFixture
 from tcutils.util import get_random_name, get_random_cidr
+from contrailapi import ContrailVncApi
 
 class GenericTestBase(test_v1.BaseTestCase_v1):
 
@@ -20,6 +21,7 @@ class GenericTestBase(test_v1.BaseTestCase_v1):
         cls.cn_inspect = cls.connections.cn_inspect
         cls.analytics_obj = cls.connections.analytics_obj
         cls.api_s_inspect = cls.connections.api_server_inspect
+        cls.vnc_h = ContrailVncApi(cls.vnc_lib, cls.logger)
         try:
             address_family = cls.address_family or 'v4'
         except AttributeError:
@@ -193,6 +195,19 @@ class GenericTestBase(test_v1.BaseTestCase_v1):
             q_h = self.quantum_h
         q_h.delete_security_group(sg_id)
 
+    def create_fip(self, fip_fixture):
+        self.logger.info('Creating FIP from %s'%fip_fixture.pool_name)
+        return self.vnc_h.create_floating_ip(fip_fixture.fip_pool_obj, fip_fixture.project_obj)
+    
+    def assoc_fip(self, fip_id, vm_id):
+        return self.vnc_h.assoc_floating_ip(fip_id, vm_id)
+
+    def disassoc_fip(self, fip_id):
+        self.vnc_h.disassoc_floating_ip(fip_id)
+    
+    def del_fip(self, fip_id):
+        self.vnc_h.delete_floating_ip(fip_id)
+
     def config_aap(self, port, prefix, prefix_len=32, mac='', aap_mode='active-standby', contrail_api=False):
         self.logger.info('Configuring AAP on port %s' %port['id'])
         if is_v6(prefix):
@@ -204,9 +219,152 @@ class GenericTestBase(test_v1.BaseTestCase_v1):
                 {"ip_address": prefix + '/' + str(prefix_len) , "mac_address": mac}]}
             port_rsp = self.update_port(port['id'], port_dict)
         return True
-    #end config_aap
+    # end config_aap
 
-    def remove_from_cleanups(self, func_call, *args):
+    def config_vrrp_on_vsrx(self, vm_fix, vip, priority):
+        cmdList = []
+        cmdList.append('deactivate security nat source rule-set TestNat')
+        cmdList.append(
+            'deactivate interfaces ge-0/0/1 unit 0 family inet filter')
+        cmdList.append('deactivate interfaces ge-0/0/1.0 family inet dhcp')
+        cmdList.append('deactivate security policies')
+        cmdList.append(
+            'set security forwarding-options family inet6 mode packet-based')
+        cmdList.append(
+            'set security forwarding-options family mpls mode packet-based')
+        cmdList.append(
+            'set security forwarding-options family iso mode packet-based')
+        vsrx_vrrp_config = ['set interfaces ge-0/0/1.0 family inet address ' + vm_fix.vm_ips[
+            1] + '/' + '24 vrrp-group 1 priority ' + priority + ' virtual-address ' + vip + ' accept-data']
+        cmdList = cmdList + vsrx_vrrp_config
+        cmd_string = (';').join(cmdList)
+        result = vm_fix.set_config_via_netconf(
+            cmd_string, timeout=10, device='junos', hostkey_verify="False")
+        return result
+    # end config_vrrp_on_vsrx
+
+    @retry(delay=5, tries=10)
+    def config_vrrp(self, vm_fix, vip, priority):
+        self.logger.info('Configuring VRRP on %s ' % vm_fix.vm_name)
+        vrrp_cmd = 'nohup vrrpd -n -D -i eth0 -v 1 -a none -p %s -d 3 %s' % (
+            priority, vip)
+        vm_fix.run_cmd_on_vm(cmds=[vrrp_cmd], as_sudo=True)
+        result = self.vrrp_chk(vm_fix)
+        return result
+    # end config_vrrp
+
+    def vrrp_chk(self, vm):
+        vrrp_chk_cmd = 'netstat -anp | grep vrrpd'
+        vm.run_cmd_on_vm(cmds=[vrrp_chk_cmd], as_sudo=True)
+        vrrp_op = vm.return_output_cmd_dict[vrrp_chk_cmd]
+        if '/vrrpd' in vrrp_op:
+            result = True
+            self.logger.info('vrrpd running in %s' % vm.vm_name)
+        else:
+            result = False
+            self.logger.error('vrrpd not running in %s' % vm.vm_name)
+        return result
+    # end vrrp_chk
+
+    @retry(delay=5, tries=10)
+    def vrrp_mas_chk(self, vm, vn, ip, vsrx=False):
+        self.logger.info(
+            'Will verify who the VRRP master is and the corresponding route entries in the Agent')
+        if is_v4(ip):
+            prefix_len = '32'
+            vrrp_mas_chk_cmd = 'ip -4 addr ls'
+        elif is_v6(ip):
+            prefix_len = '128'
+            vrrp_mas_chk_cmd = 'ip -6 addr ls'
+
+        if vsrx:
+            vrrp_mas_chk_cmd = 'show vrrp'
+            result = vm.get_config_via_netconf(
+                cmd=vrrp_mas_chk_cmd, timeout=10, device='junos', hostkey_verify="False", format='text')
+            if 'master' in result:
+                self.logger.info(
+                    '%s is selected as the VRRP Master' % vm.vm_name)
+                result = True
+            else:
+                result = False
+                self.logger.error('VRRP Master not selected')
+        else:
+            vm.run_cmd_on_vm(cmds=[vrrp_mas_chk_cmd], as_sudo=True)
+            output = vm.return_output_cmd_dict[vrrp_mas_chk_cmd]
+            result = False
+            if ip in output:
+                self.logger.info(
+                    '%s is selected as the VRRP Master' % vm.vm_name)
+                result = True
+            else:
+                result = False
+                self.logger.error('VRRP Master not selected')
+        result = result and self.check_master_in_agent(vm, vn, ip, prefix_len)
+        return result
+    # end vrrp_mas_chk
+
+    @retry(delay=3, tries=5)
+    def check_master_in_agent(self, vm, vn, ip, prefix_len='32', ecmp=False):
+        inspect_h = self.agent_inspect[vm.vm_node_ip]
+        (domain, project, vnw) = vn.vn_fq_name.split(':')
+        agent_vrf_objs = inspect_h.get_vna_vrf_objs(domain, project, vnw)
+        agent_vrf_obj = vm.get_matching_vrf(
+            agent_vrf_objs['vrf_list'], vn.vrf_name)
+        vn1_vrf_id = agent_vrf_obj['ucindex']
+        result = False
+        paths = []
+        try:
+            paths = inspect_h.get_vna_active_route(
+                vrf_id=vn1_vrf_id, ip=ip, prefix=prefix_len)['path_list']
+        except TypeError:
+            self.logger.info('Unable to retreive path info')
+        for path in paths:
+            if path['peer'] == 'LocalVmPort' and path['path_preference_data']['wait_for_traffic'] == 'false':
+                result = True
+                if ecmp:
+                    if path['path_preference_data']['ecmp'] == 'true':
+                        result = True
+                        break
+                    else:
+                        result = False
+                        return result
+                else:
+                    break
+            else:
+                result = False
+        return result
+    # end check_master_in_agent
+
+    @retry(delay=5, tries=10)
+    def verify_vrrp_action(self, src_vm, dst_vm, ip, vsrx=False):
+        result = False
+        self.logger.info('Will ping %s from %s and check if %s responds' % (
+            ip, src_vm.vm_name, dst_vm.vm_name))
+        compute_ip = dst_vm.vm_node_ip
+        compute_user = self.inputs.host_data[compute_ip]['username']
+        compute_password = self.inputs.host_data[compute_ip]['password']
+        session = ssh(compute_ip, compute_user, compute_password)
+        if vsrx:
+            vm_tapintf = dst_vm.tap_intf[dst_vm.vn_fq_names[1]]['name']
+        else:
+            vm_tapintf = dst_vm.tap_intf[dst_vm.vn_fq_name]['name']
+        cmd = 'tcpdump -nni %s -c 10 > /tmp/%s_out.log' % (
+            vm_tapintf, vm_tapintf)
+        execute_cmd(session, cmd, self.logger)
+        assert src_vm.ping_with_certainty(ip), 'Ping to vIP failure'
+        output_cmd = 'cat /tmp/%s_out.log' % vm_tapintf
+        output, err = execute_cmd_out(session, output_cmd, self.logger)
+        if ip in output:
+            result = True
+            self.logger.info(
+                '%s is seen responding to ICMP Requests' % dst_vm.vm_name)
+        else:
+            self.logger.error('ICMP Requests not seen on the VRRP Master')
+            result = False
+        return result
+    # end verify_vrrp_sction
+
+    def _remove_from_cleanup(self, func_call, *args):
         for cleanup in self._cleanups:
             if func_call in cleanup and args == cleanup[1]:
                 self._cleanups.remove(cleanup)
