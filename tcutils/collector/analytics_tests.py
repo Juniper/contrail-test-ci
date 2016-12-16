19a0e26fadc82d781416dc2f7a97891e57a0a319
ankitja <ankitja@juniper.net>
2016-12-15 23:26:12 -0800

    Fix for alarm test cases on Centos
        Split hostname before passing it to get_alarms
    
    Change-Id: Ia74064fa0ccaaa39ff2b4f600f4004c68f2d2300

--- tcutils/collector/analytics_tests.py
+++ tcutils/collector/analytics_tests.py
@@ -2874,6 +2874,7 @@ class AnalyticsVerification(fixtures.Fixture):
             hostname = alarm_name
         else:
             hostname = self.inputs.host_data[service_ip]['name']
+            hostname = hostname.split('.')[0]
         if not isinstance(alarm_type, list):
             alarm_type = [alarm_type]
         try:
