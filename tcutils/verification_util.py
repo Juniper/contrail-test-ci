8a5555b7ed4c82060f4a6afda5d856e91845ec31
usha <musharani@juniper.net>
2016-12-16 15:40:01 +0530

    Modified scripts to support SSL
    Change-Id: I7e540e523dd17e7352cfd36d3eaacdc0d29b81b9

--- tcutils/verification_util.py
+++ tcutils/verification_util.py
@@ -3,6 +3,8 @@ import json
 import urllib2
 import requests
 import logging as LOG
+from tcutils.util import *
+from cfgm_common import utils
 from lxml import etree
 
 LOG.basicConfig(format='%(levelname)s: %(message)s', level=LOG.INFO)
@@ -20,6 +22,13 @@ class JsonDrv (object):
         self._headers = None
         self._args = args
         self._use_admin_auth = use_admin_auth
+        if self._args and self._args.api_protocol == 'https':
+            self.api_bundle = '/tmp/' + get_random_string() + '.pem'
+            if self._args.apicertfile and self._args.apikeyfile and \
+                   self._args.apicafile and not self._args.api_insecure:
+                self.certs=[self._args.apicertfile, self._args.apikeyfile,
+                           self._args.apicafile]
+                self.apicertbundle=utils.getCertKeyCaBundle(self.api_bundle, self.certs)
 
     def _auth(self):
         if self._args:
@@ -28,10 +37,12 @@ class JsonDrv (object):
             else:
                 url = "%s://%s:%s%s" % (self._args.auth_protocol,
                                         self._args.auth_ip,
-                                        self._args.auth_port, 
+                                        self._args.auth_port,
                                         self._DEFAULT_AUTHN_URL)
-            insecure = bool(os.getenv('OS_INSECURE',True))
-            verify = not insecure
+            if self._args.insecure:
+                verify = not self._args.insecure
+            else:
+                verify = self._args.keycertbundle
             self._authn_body = \
                 '{"auth":{"passwordCredentials":{"username": "%s", "password": "%s"}, "tenantName":"%s"}}' % (
                     self._args.admin_username if self._use_admin_auth else self._args.stack_user,
@@ -50,7 +61,10 @@ class JsonDrv (object):
 
     def load(self, url, retry=True):
         self.log.debug("Requesting: %s", url)
-        resp = requests.get(url, headers=self._headers)
+        if url.startswith('https:'):
+            resp = requests.get(url, headers=self._headers, verify=self.apicertbundle)
+        else:
+            resp = requests.get(url, headers=self._headers)
         if resp.status_code == 401:
             if retry:
                 self._auth()
@@ -82,7 +96,8 @@ class XmlDrv (object):
 
 class VerificationUtilBase (object):
 
-    def __init__(self, ip, port, drv=JsonDrv, logger=LOG, args=None, use_admin_auth=False):
+    def __init__(self, ip, port, drv=JsonDrv, logger=LOG, args=None, use_admin_auth=False,
+                 protocol='http'):
         self.log = logger
         self._ip = ip
         self._port = port
@@ -97,9 +112,10 @@ class VerificationUtilBase (object):
         return self.get_force_refresh()
 
     def _mk_url_str(self, path=''):
-        if path.startswith('http:'):
+        if path.startswith('http' or 'https'):
             return path
-        return "http://%s:%s/%s" % (self._ip, str(self._port), path)
+        else:
+            return self._protocol + "://%s:%s/%s" % (self._ip, str(self._port), path)
 
     def dict_get(self, path=''):
         try:
