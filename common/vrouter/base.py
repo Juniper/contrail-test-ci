46dd3c0edca475121e3d5a5306d0050c4e69a1d0
Senthilnathan Murugappan <msenthil@juniper.net>
2016-12-16 11:24:14 -0800

    Add support for RBAC and add few rbac testcases shared externally Change testbed_generator.py to support separate analytics and analytics-db mirantis roles Add support to execute single testcase from testrunner.sh
    Conflicts:
    	common/vdns/base.py
    	fixtures/contrailapi.py
    	fixtures/interface_route_table_fixture.py
    	fixtures/qos_fixture.py
    	fixtures/quantum_test.py
    	fixtures/virtual_router_fixture.py
    	fixtures/vn_test.py
    	fixtures/vnc_api_test.py
    	scripts/vdns/test_vdns_basic.py
    	tcutils/config/vnc_introspect_utils.py
    	tcutils/verification_util.py
    	testrunner.sh
    
    Change-Id: I749d57c5210a43446966607ac530a26ad51f3e20

--- common/vrouter/base.py
+++ common/vrouter/base.py
@@ -8,9 +8,9 @@ from tcutils.traffic_utils.base_traffic import *
 from tcutils.traffic_utils.hping_traffic import Hping3
 from tcutils.traffic_utils.ping_traffic import Ping
 from common.neutron.base import BaseNeutronTest
-from contrailapi import ContrailVncApi
 import random
 from security_group import get_secgrp_id_from_name, SecurityGroupFixture
+from contrailapi import ContrailVncApi
 
 class BaseVrouterTest(BaseNeutronTest):
 
