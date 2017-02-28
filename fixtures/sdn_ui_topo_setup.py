20951b24db66bcaf656df9b47717331deeb95aee
Pavana <pavanap@juniper.net>
2017-02-28 21:22:51 +0530

    Adding new test case support for Network Route Table on UI
    Change-Id: I0a749b891537f3218475126bb9c3979451e7b64d

--- fixtures/sdn_ui_topo_setup.py
+++ fixtures/sdn_ui_topo_setup.py
@@ -233,6 +233,11 @@ class sdnUiTopoSetupFixture(fixtures.Fixture):
         return True
     # end create_alarms
 
+    def create_network_route_table(self):
+        assert ui_topo_steps.createNetworkRouteTable(self)
+        return True
+    # end create_network_route_table
+
     def cleanUp(self):
         if self.inputs.fixture_cleanup == 'yes':
             super(sdnUiTopoSetupFixture, self).cleanUp()
