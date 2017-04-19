0f182a7297d40f6560f876cb8e76860cb2454d72
usha <musharani@juniper.net>
2017-04-19 15:01:54 +0530

    Added container as a param in update_js_config proc to work containerized contrail
    Change-Id: I577d44683d7b7b693cab5dbce1831dcc8befdfd4

--- tools/configure.py
+++ tools/configure.py
@@ -638,7 +638,7 @@ def configure_test_env(contrail_fab_path='/opt/contrail/utils', test_dir='/contr
                              'token', 'expiration',
                              '86400','keystone')
         update_js_config('openstack', '/etc/contrail/config.global.js',
-                         'contrail-webui')
+                         'contrail-webui', container="webui")
 
 def testbed_format_conversion(path='/opt/contrail/utils'):
     tb_file = path + '/fabfile/testbeds/testbed.py'
