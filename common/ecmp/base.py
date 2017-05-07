11caa2d76d9d3d875871f3b5e145b106213232c6
ankitja <ankitja@juniper.net>
2017-05-07 02:46:10 -0700

    Adding get_svm_count in ecmp/base.py
    Change-Id: Id937c2c3bfca7f54bfa9985ee549397fc5f44114

--- common/ecmp/base.py
+++ common/ecmp/base.py
@@ -78,3 +78,17 @@ class ECMPTestBase(GenericTestBase):
         super(ECMPTestBase, cls).tearDownClass()
     # end tearDownClass
 
+    @retry(delay=1, tries=30)
+    def get_svm_count(self, expected_count):
+        svms = self.get_svms_in_si(
+            self.si_fixtures[0], self.inputs.project_name)
+        svms = sorted(set(svms))
+        svms = filter(None, svms)
+        if len(svms) == expected_count:
+            self.logger.info('The Service VMs in the Service Instance %s are %s' % (
+                self.si_fixtures[0].si_name, svms))
+            return True, svms
+        self.logger.warn('The SVMs count has not decreased..retrying')
+        return False, False
+    # end test_ecmp_with_svm_deletion
+
