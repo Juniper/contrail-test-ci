f855526272cf0e1fad271b104a4dfc9dca92a4c1
Senthilnathan Murugappan <msenthil@juniper.net>
2017-05-08 15:08:47 -0700

    When setupClass fails we need to cleanup the objects hence capturing the exception and calling the cleanup of objects created during setupclass
    Change-Id: I4f30262eb99de96c930ee22e1787cdb7ac9cb410
    
    rbac changes to support subfield, ownership changes and remove plural objects
    
    Change-Id: I2b3fdfa1b3ff24d008b842ae81aa4121fe14ec98
    Closes-Bug: 1721438

--- tcutils/topo/topo_steps.py
+++ tcutils/topo/topo_steps.py
@@ -92,7 +92,6 @@ def create_sg_quantum(self):
             msg = []
             self.secgrp_fixture[sg_name] = self.useFixture(
                 SecurityGroupFixture(
-                    inputs=self.inputs,
 		    connections=self.project_connections,
                     domain_name=self.topo.domain,
                     project_name=self.topo.project,
@@ -115,7 +114,6 @@ def create_sg_contrail(self):
             msg = []
             self.secgrp_fixture[sg_name] = self.useFixture(
                 SecurityGroupFixture(
-                    inputs=self.inputs,
                     connections=self.project_connections,
                     domain_name=self.topo.domain,
                     project_name=self.topo.project,
