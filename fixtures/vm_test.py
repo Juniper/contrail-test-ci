6975008c162a3a9c6ee938ffd2b9a8dd2cbdb4c6
Alok Kumar <kalok@juniper.net>
2017-07-27 22:40:34 +0530

    Revert back IP verification on VM
        1. Remove IP verification from wait_for_ssh_on_vm
        2. Add a new method wait_till_interface_created
    
    Change-Id: I5dd9006e5a043ca810e624f56d8e63e436f4811e
    Partial-Bug: #1705635

--- fixtures/vm_test.py
+++ fixtures/vm_test.py
@@ -2438,7 +2438,6 @@ class VMFixture(fixtures.Fixture):
     def wait_for_ssh_on_vm(self):
         self.logger.debug('Waiting to SSH to VM %s, IP %s' % (self.vm_name,
                                                               self.vm_ip))
-        result = False
         host = self.inputs.host_data[self.vm_node_ip]
         vm_hoststring = '@'.join([self.vm_username, self.local_ip])
         if sshable(vm_hoststring, self.vm_password,
@@ -2447,16 +2446,11 @@ class VMFixture(fixtures.Fixture):
                    logger=self.logger):
             self.logger.debug('VM %s is ready for SSH connections'
                               % self.vm_name)
-            result = True
+            return True
         else:
             self.logger.debug('VM %s is NOT ready for SSH connections'
                               % self.vm_name)
             return False
-
-        #Check if interface on the VM has the ip assigned
-        result = True if self.get_vm_interface_list(ip=self.vm_ip) else False
-
-        return result
     # end wait_for_ssh_on_vm
 
     def copy_file_to_vm(self, localfile, dstdir=None, force=False):
@@ -2817,6 +2811,22 @@ class VMFixture(fixtures.Fixture):
         self._vm_interface[mac_address] = name
         return name
     # end get_vm_interface_name
+
+    @retry(delay=2, tries=10)
+    def wait_till_interface_created(self, interface, ip=None):
+        '''
+        Wait till interface is found on VM and it has the given ip assigned
+        '''
+        interface_list = self.get_vm_interface_list(ip=ip)
+
+        if interface in interface_list:
+            self.logger.info("Interface %s is found on VM %s" % (interface,
+                self.vm_id))
+            return (True, None)
+        else:
+            self.logger.warn("Interface %s is not found on VM %s" % (interface,
+                self.vm_id))
+            return (False, None)
 
     def get_vm_interface_list(self, ip=None):
         '''if ip is None, returns all interfaces list.
