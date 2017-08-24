8cd93a712a42b2775e4d07495447c58cf123d26c
Sunil Basker <sunilbasker@juniper.net>
2017-08-24 07:53:50 +0530

    Correct to standard indentation

--- api_drivers/openstack/vm.py
+++ api_drivers/openstack/vm.py
@@ -1,44 +1,44 @@
 import copy
 
 try:
-   from novaclient import exceptions as novaException
+    from novaclient import exceptions as novaException
 
-   class OsVmMixin:
+    class OsVmMixin:
 
-       ''' Mixin class implements CRUD methods for virtual machine
-       '''
+        ''' Mixin class implements CRUD methods for virtual machine
+        '''
 
-       def create_virtual_machine (self, **kwargs):
-           assert args['type'] == 'openstack', "Unsupport argument type"
-           args = copy.deepcopy(kwargs)
-           del args['type']
-           lst = []
-           for nic in args['networks']:
-               if nic.keys()[0] == 'port':
-                   lst.append({'port-id': nic.values()[0]})
-               else:
-                   lst.append({'net-id': nic.values()[0]})
-           args['nics'] = lst
-           del args['networks']
-           obj = self._nh.servers.create(**args)
-           return obj.id
+        def create_virtual_machine (self, **kwargs):
+            assert args['type'] == 'openstack', "Unsupport argument type"
+            args = copy.deepcopy(kwargs)
+            del args['type']
+            lst = []
+            for nic in args['networks']:
+                if nic.keys()[0] == 'port':
+                    lst.append({'port-id': nic.values()[0]})
+                else:
+                    lst.append({'net-id': nic.values()[0]})
+            args['nics'] = lst
+            del args['networks']
+            obj = self._nh.servers.create(**args)
+            return obj.id
 
-       def get_virtual_machine (self, uuid):
-           ret = self._nh.servers.list(search_opts={'uuid':uuid})
-           if ret:
-               return ret[0]
-           return None
+        def get_virtual_machine (self, uuid):
+            ret = self._nh.servers.list(search_opts={'uuid':uuid})
+            if ret:
+                return ret[0]
+            return None
 
-       def delete_virtual_machine (self, obj=None, uuid=None):
-           uuid = uuid or obj.id
-           self._nh.servers.delete(uuid)
+        def delete_virtual_machine (self, obj=None, uuid=None):
+            uuid = uuid or obj.id
+            self._nh.servers.delete(uuid)
 
-       def update_virtual_machine (self, obj=None, uuid=None, **kwargs):
-           assert args['type'] == 'openstack', "Unsupport argument type"
-           args = copy.deepcopy(kwargs)
-           del args['type']
-           pass #TODO
+        def update_virtual_machine (self, obj=None, uuid=None, **kwargs):
+            assert args['type'] == 'openstack', "Unsupport argument type"
+            args = copy.deepcopy(kwargs)
+            del args['type']
+            pass #TODO
 
 except ImportError:
-   class OsVmMixin:
-       pass
+    class OsVmMixin:
+        pass
