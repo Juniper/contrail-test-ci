ee86caccbcb98624aea5e3d28ab98c99f9bf37a8
sarath <nsarath@juniper.net>
2017-10-26 18:31:13 -0700

    add test_attr vcenter_compute
    add test_attr vcenter_compute to exclude unsupported and include required tests like additional checks to verify ping across multi-cluster esxi and kvms.
    
    Change-Id: I4253eda99910eb96bef09e46493813f44e7d1da8
    Closes-Bug: #1727892

--- scripts/vm_regression/test_vm_basic.py
+++ scripts/vm_regression/test_vm_basic.py
@@ -35,7 +35,7 @@ class TestBasicVMVN(BaseVnVmTest):
     #end runTes
 
     @skip_because(orchestrator = 'vcenter', hypervisor='docker',msg='Bug 1455944:VM image with cloud-init package needed')
-    @test.attr(type=['cb_sanity', 'sanity', 'ci_sanity', 'suite1'])
+    @test.attr(type=['cb_sanity', 'sanity', 'ci_sanity', 'suite1', 'vcenter_compute'])
     @preposttest_wrapper
     @skip_because(orchestrator = 'vcenter')
     def test_metadata_service(self):
@@ -146,7 +146,7 @@ echo "Hello World.  The time is now $(date -R)!" | tee /tmp/output.txt
         return True
 
 
-    @test.attr(type=['sanity', 'suite1', 'ci_sanity'])
+    @test.attr(type=['sanity', 'suite1', 'ci_sanity', 'vcenter_compute'])
     @preposttest_wrapper
     @skip_because(orchestrator = 'vcenter',address_family = 'v6',
         hypervisor='docker',msg='Bug 1461423:Need privileged access')
@@ -312,7 +312,7 @@ echo "Hello World.  The time is now $(date -R)!" | tee /tmp/output.txt
         return True
     # end test_ping_within_vn
 
-    @test.attr(type=['cb_sanity', 'sanity', 'ci_sanity', 'vcenter', 'suite1'])
+    @test.attr(type=['cb_sanity', 'sanity', 'ci_sanity', 'vcenter', 'suite1', 'vcenter_compute'])
     @preposttest_wrapper
     @skip_because(orchestrator = 'vcenter',address_family = 'v6')
     def test_generic_link_local_service(self):
