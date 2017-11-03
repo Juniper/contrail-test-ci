7fe3ba81aab3d0f5c2877fe73d6081e9aa22456e
ankitja <ankitja@juniper.net>
2017-11-03 17:08:06 +0530

    Adding check_test_contrail_status.sh
    Change-Id: I3cb39971e72e727be2ad96825f53f39e06ac9641
    Closes-bug: #1729837

---
+++ tools/check_test_contrail_status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Validating if all the contrail services are up and running"
+echo ""
+
+PYTHONPATH=$PATH:$PWD/scripts:$PWD/fixtures python -m testtools.run scripts.analytics.test_analytics.AnalyticsTestSanity.test_contrail_status
+
+if [ $? != 0 ]
+then
+    echo "One or more contrail services not up"
+    exit 1
+else
+    echo "All the contrail services are up"
+fi
