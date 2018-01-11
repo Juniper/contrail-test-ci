68dfeb1aab89f7cc5f2b671af32a57452c953be6
Senthilnathan Murugappan <msenthil@juniper.net>
2018-01-10 19:54:29 -0800

    Changing image name to ubuntu
    Making the image name and ping size consistent across all the branches
    
    Change-Id: Ie9a011dbdb72489edc02a4f71a424dc8657bc9f6
    Closes-Bug: #1731078

--- common/intf_mirroring/verify.py
+++ common/intf_mirroring/verify.py
@@ -307,7 +307,7 @@ class VerifyIntfMirror(VerifySvcMirror):
         analyzer_compute = compute_nodes[2]
 
         analyzer_port = 8099
-        image_name = 'cirros' if not sub_intf else 'ubuntu-traffic'
+        image_name = 'ubuntu' if not sub_intf else 'ubuntu-traffic'
         if self.inputs.pcap_on_vm:
             image_name='ubuntu-traffic'
 
